52224f8498f71178d503d2b6bff686eba1f69296
mcpal bia <mirandacpalumbo@gmail.com>
2024-10-06 15:16:26 -0300

    docker foldseek

--- setup_docker.sh
+++ setup_docker.sh
@@ -6,5 +6,6 @@ docker pull ezequieljsosa/fpocket
 docker pull brinkmanlab/psortb_commandline:1.0.2
 docker pull mcpalumbo/corecruncher:1
 docker pull mcpalumbo/bioperl:1
+docker pull mcpalumbo/foldseek:1
 
 echo "Docker images setup completed."
