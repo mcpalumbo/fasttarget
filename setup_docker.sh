93b98231770a6312d848848d5f7fae268e5e20fe
mcpal bia <mirandacpalumbo@gmail.com>
2025-11-20 14:35:18 -0300

    p2rank new docker

--- setup_docker.sh
+++ setup_docker.sh
@@ -2,10 +2,11 @@
 
 # Pull Docker images
 docker pull sangerpathogens/roary
-docker pull fpocket/fpocket
+docker pull ezequieljsosa/fpocket
 docker pull brinkmanlab/psortb_commandline:1.0.2
 docker pull mcpalumbo/corecruncher:1
 docker pull mcpalumbo/bioperl:1
 docker pull mcpalumbo/foldseek:1
+docker pull mcpalumbo/p2rank:latest
 
 echo "Docker images setup completed."
