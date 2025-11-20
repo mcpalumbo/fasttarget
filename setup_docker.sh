e227bd63f78386223c4f756e71f986ccdd5a6a96
mcpal bia <mirandacpalumbo@gmail.com>
2025-11-20 14:36:19 -0300

    new version fpocket

--- setup_docker.sh
+++ setup_docker.sh
@@ -2,7 +2,7 @@
 
 # Pull Docker images
 docker pull sangerpathogens/roary
-docker pull ezequieljsosa/fpocket
+docker pull fpocket/fpocket
 docker pull brinkmanlab/psortb_commandline:1.0.2
 docker pull mcpalumbo/corecruncher:1
 docker pull mcpalumbo/bioperl:1
