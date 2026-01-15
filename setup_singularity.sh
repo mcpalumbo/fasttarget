84d1513dec59b47cc29324cc5cbd73404cdbac29
mcpal bia <mirandacpalumbo@gmail.com>
2026-01-15 00:53:29 -0300

    all .sif in singularity_sfi_files folder

--- setup_singularity.sh
+++ setup_singularity.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-singularity pull docker://sangerpathogens/roary
-singularity pull docker://fpocket/fpocket
-singularity pull docker://brinkmanlab/psortb_commandline:1.0.2
-singularity pull docker://mcpalumbo/corecruncher:1
-singularity pull docker://mcpalumbo/bioperl:1
-singularity pull docker://mcpalumbo/foldseek:1
-singularity pull docker://mcpalumbo/p2rank:latest
-singularity pull docker://mcpalumbo/metagraphtools:latest
+# Create directory for Singularity .sif files
+mkdir -p singularity_sfi_files
 
-echo "Singularity containers have been pulled successfully."
+# Pull containers with specific names matching what the code expects
+singularity pull --name singularity_sfi_files/sangerpathogens_roary.sif docker://sangerpathogens/roary
+singularity pull --name singularity_sfi_files/fpocket_fpocket.sif docker://fpocket/fpocket
+singularity pull --name singularity_sfi_files/brinkmanlab_psortb_commandline_1.0.2.sif docker://brinkmanlab/psortb_commandline:1.0.2
+singularity pull --name singularity_sfi_files/mcpalumbo_corecruncher_1.sif docker://mcpalumbo/corecruncher:1
+singularity pull --name singularity_sfi_files/mcpalumbo_bioperl_1.sif docker://mcpalumbo/bioperl:1
+singularity pull --name singularity_sfi_files/mcpalumbo_foldseek_1.sif docker://mcpalumbo/foldseek:1
+singularity pull --name singularity_sfi_files/mcpalumbo_p2rank_latest.sif docker://mcpalumbo/p2rank:latest
+singularity pull --name singularity_sfi_files/mcpalumbo_metagraphtools_latest.sif docker://mcpalumbo/metagraphtools:latest
+
+echo "Singularity containers have been pulled successfully to singularity_sfi_files/"
+
