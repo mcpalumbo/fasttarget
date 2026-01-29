b0ff218a7ada3a090354473fce7679f04784540c
mcpal bia <mirandacpalumbo@gmail.com>
2026-01-29 02:44:37 -0300

    import update

--- ftscripts/structures.py
+++ ftscripts/structures.py
@@ -14,6 +14,7 @@ import databases
 import glob
 import shutil
 import logging
+import json
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import requests
 import xml.etree.ElementTree as ET
